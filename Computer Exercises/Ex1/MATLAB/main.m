e243cbedde87c3457b90b434505e4d8e04000bda
Sifan Jiang <sifanj@kth.se>
2019-03-23 20:49:14 +0100

    Working on 4.1.1

--- Computer Exercises/Ex1/MATLAB/main.m
+++ Computer Exercises/Ex1/MATLAB/main.m
@@ -1,1 +1,24 @@
 close all; clear; clc;
+
+s = tf('s');
+G = 3*(-s+1)/((5*s+1)*(10*s+1));
+F = 1;
+
+% S = feedback(eye(2), G*F);
+% T = feedback(G*F, eye(2));
+
+S = 1/(1+G*F);
+T = G*F/(1+G/F);
+
+figure
+bode(G)
+
+[m,p] = bode(G,1);
+
+[Gm,Pm,wp,wc] = margin(G*F);
+
+figure
+step(G)
+
+figure
+step(T)
